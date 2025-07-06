3d8dccea48c3669461665f9532d838e8cb23d7df
Full-Hat <nikita3131228@gmail.com>
2025-07-06 05:29:16 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Empty GLFW Window", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
